057b8fdebb84dfced76561558309328cb725c0e1
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2023-09-24 07:36:16 -0700

    fix for home dir

---
+++ transfer_to_baker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ./common.sh
+
+# Prompt the user for the amount to transfer
+read -p "Enter the amount to transfer: " amount
+
+# Iterate over the list of addresses
+for address in "${staker_addresses[@]}"; do
+    # Use the octez-client command to transfer tez from the address to the target address
+    $HOME/tezos/octez-client transfer $amount from $address to $baker_address >/dev/null
+
+    # Print a message indicating that the transfer has been initiated
+    echo -e "${COLORS[GREEN]}Initiated a transfer of ${COLORS[NC]}$amount ${COLORS[GREEN]}from ${COLORS[YELLOW]}$address ${COLORS[GREEN]}to ${COLORS[YELLOW]}$baker_address.${COLORS[NC]}"
+done
